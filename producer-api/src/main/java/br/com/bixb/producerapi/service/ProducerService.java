2d8cb0de7258de1bd7114059bf65a9d1aa6436de
Vinícius Carneiro de Brito <viniciuscbrito89@gmail.com>
2022-05-24 08:37:04 -0300

    Spring kafka config

--- producer-api/src/main/java/br/com/bixb/producerapi/service/ProducerService.java
+++ producer-api/src/main/java/br/com/bixb/producerapi/service/ProducerService.java
@@ -1,6 +1,5 @@
 package br.com.bixb.producerapi.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
